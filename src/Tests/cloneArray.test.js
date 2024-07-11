d9061f6e76c6397b447b28d452271a0030e28111
geraldombuthia <geraldombuthia@gmail.com>
2024-07-11 20:49:40 +0300

    f/implement_integrated_test_foundation: Fixed issue of jest failing eslint

--- src/Tests/cloneArray.test.js
+++ src/Tests/cloneArray.test.js
@@ -1,6 +1,5 @@
 const cloneArray = require("../cloneArray");
 
-
 test("Clones an Array", () => {
     expect(cloneArray([1,2,3])).toEqual([1,2,3]);
-})
+});
--- src/Tests/sum.test.js
+++ src/Tests/sum.test.js
@@ -1,4 +1,4 @@
-const sum = require('../sum.js');
+const sum = require("../sum.js");
 
 test("Adds two numbers", () => {
     expect(sum(1,2)).toBe(3);
