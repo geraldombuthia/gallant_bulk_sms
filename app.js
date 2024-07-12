6047944ecc5b6aec3214b8ba45294a5486d1912b
geraldombuthia <geraldombuthia@gmail.com>
2024-07-12 22:24:43 +0300

    f/extract_headers_from_request: Extracted headers especially UserAgent info

--- app.js
+++ app.js
@@ -4,6 +4,12 @@ const app = express();
 const port = 3000;
 
 app.route("/").get((req, res) => {
+    // console.log(req.headers);
+    const {host, connection, "cache-control": cacheControl, "user-agent":userAgent} = req.headers;
+    // console.log(req.headers);
+    console.log(host, connection, cacheControl, userAgent);
+
+    console.log("\n\n" + userAgent);
     res.status(200).json({msg:"Welcome to Gallant Byte SMS"});
 });
 
