f529669f1c04a3fa3e004d942650edde341c5b9b
geraldombuthia <geraldombuthia@gmail.com>
2024-08-06 22:11:59 +0300

    f/add-login-and-registration: Implemented basic conditional rendering of the Dashboard and logout buttons once a user has logged in

--- app.js
+++ app.js
@@ -50,6 +50,11 @@ app.use(session({
 }))
 app.use(passport.initialize());
 app.use(passport.session());
+app.use((req, res, next) => {
+    res.locals.user = req.user;
+    next();
+});
+
 app.use('/auth', AuthRoutes);
 
 // app.use(skipFaviconMiddleware, userAgentParser, extractIPAddress, logDeviceAccess); // middleware 
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -3,11 +3,18 @@ const passport = require('../config/passport');
 const AuthController = require('../controllers/auth.controller');
 
 const router = express.Router();
-router.get('/register', (req, res) => {res.render('register');});
+router.get('/register', (req, res) => {res.render('register', {user: req.user});});
 router.post('/register', AuthController.register);
 router.get('/login', (req, res) => {res.render('login');})
 router.post('/login', passport.authenticate('local'), AuthController.login);
-router.get('/logout', AuthController.logout, (req, res) => {res.json({message: "Hey logged out"});});
+router.get('/logout', AuthController.logout, (req, res) => {
+    req.logout((err) => {
+        if (err) {
+            return next(err);
+        }
+        res.redirect('/'); // Redirect to the home page
+    })
+});
 
 module.exports = router;
 
