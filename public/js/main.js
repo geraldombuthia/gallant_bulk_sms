91a3ae12f2b265c52e09bd87eeea77207e1850fd
geraldombuthia <geraldombuthia@gmail.com>
2024-08-13 21:32:21 +0300

    f/implement-basic-dashboard: Uncommented main.js items that were thought to not work

--- public/js/main.js
+++ public/js/main.js
@@ -7,22 +7,22 @@ document.addEventListener("DOMContentLoaded", () => {
         navMenu.classList.toggle("active");
     });
 
-    //   // Close menu when a link is clicked
-    // document.querySelectorAll(".nav-menu a").forEach((link) => {
-    //     link.addEventListener("click", (e) => {
-    //         navToggle.classList.remove("active");
-    //         navMenu.classList.remove("active");
+      // Close menu when a link is clicked
+    document.querySelectorAll(".nav-menu a").forEach((link) => {
+        link.addEventListener("click", (e) => {
+            navToggle.classList.remove("active");
+            navMenu.classList.remove("active");
 
-    //         if (link.getAttribute("href").startsWith("Auth")) {
-    //             return;
-    //         }
+            if (link.getAttribute("href").startsWith("Auth")) {
+                return;
+            }
 
-    //         // if (link.getAttribute("href").startsWith("#")) {
-    //         //     e.preventDefault();
-    //         //     const hash = link.getAttribute("href");
-    //         //     console.log("Hash: ", hash);
-    //         //     window.location.href = `/${hash}`;
-    //         // }
-    //     });
-    // });
+            if (link.getAttribute("href").startsWith("#")) {
+                e.preventDefault();
+                const hash = link.getAttribute("href");
+                console.log("Hash: ", hash);
+                window.location.href = `/${hash}`;
+            }
+        });
+    });
 });
