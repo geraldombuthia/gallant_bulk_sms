de04c2b99636af385cea8e387c2fef4c00e0db41
geraldombuthia <geraldombuthia@gmail.com>
2025-01-12 12:31:39 +0300

    f/introduce_docker_containerization: Fixed linting issues

--- src/config/config.js
+++ src/config/config.js
@@ -6,4 +6,4 @@ module.exports= {
         host: process.env.DB_HOST,
         dialect: "mysql"
     },
-}
+};
--- src/config/database.js
+++ src/config/database.js
@@ -7,7 +7,7 @@ const sequelize = new Sequelize(
     process.env.DB_USERNAME,
     process.env.DB_PASSWORD,
     {
-        host: process.env.DB_HOST, //'gallantbulkdb' for use with docker,
+        host: process.env.DB_HOST, // 'gallantbulkdb' for use with docker,
         port: 3306,
         dialect: process.env.DB_DIALECT,
 
