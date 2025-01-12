9671bcfb34b0b1c541b10abace4a91003a001b7b
geraldombuthia <geraldombuthia@gmail.com>
2025-01-12 12:26:26 +0300

    f/introduce_docker_containerization: Removed debug statement

--- src/config/passport.js
+++ src/config/passport.js
@@ -3,43 +3,37 @@ const LocalStrategy = require("passport-local").Strategy;
 const User = require("../models/users.js");
 
 passport.use(
-  new LocalStrategy(
-    { usernameField: "email" },
-    async (email, password, done) => {
-      try {
-        console.log(
-          "This is the user data",
-          User.findOne({
-            where: { email: "geraldombuthia@gmail.com" },
-          })
-        );
+    new LocalStrategy(
+        { usernameField: "email" },
+        async (email, password, done) => {
+            try {
+                const user = await User.findOne({ where: { email } });
 
-        const user = await User.findOne({ where: { email } });
+                if (!user) {
+                    return done(null, false, { message: "Incorrect email." });
 
-        if (!user) {
-          return done(null, false, { message: "Incorrect email." });
+                }
+                const isMatch = await user.validatePassword(password);
+                if (!isMatch) {
+                    return done(null, false, { message: "Incorrect password" });
+                }
+                return done(null, user);
+            } catch (error) {
+                return done(error);
+            }
         }
-        const isMatch = await user.validatePassword(password);
-        if (!isMatch) {
-          return done(null, false, { message: "Incorrect password" });
-        }
-        return done(null, user);
-      } catch (error) {
-        return done(error);
-      }
-    }
-  )
+    )
 );
 passport.serializeUser((user, done) => {
-  done(null, user.id);
+    done(null, user.id);
 });
 passport.deserializeUser(async (id, done) => {
-  try {
-    const user = await User.findByPk(id);
-    done(null, user);
-  } catch (error) {
-    done(error);
-  }
+    try {
+        const user = await User.findByPk(id);
+        done(null, user);
+    } catch (error) {
+        done(error);
+    }
 });
 
 module.exports = passport;
