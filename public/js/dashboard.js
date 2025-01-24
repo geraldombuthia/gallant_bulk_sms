89fab5425c0b35bcd9f2f9d8df4087dc07ecd296
geraldombuthia <geraldombuthia@gmail.com>
2025-01-25 00:08:17 +0300

    f/implement_user_dashboard_home_page: Added success and error pop ups

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,29 +1,7 @@
-const editProfileBtn = document.getElementById("edit-profile-btn");
-const profileEditOverlay = document.getElementById("profile-edit-overlay");
-const profileEditCloseButton = document.querySelector("#profile-edit-overlay .close-button");
-const profileEditForm = document.getElementById("profile-edit-form");
-
-// Security Settings
-const updateSecurityBtn = document.getElementById("update-security-btn");
-const securityOverlay = document.getElementById("security-overlay");
-const securityCloseButton = document.querySelector(
-    "#security-overlay .close-button"
-);
-const securityForm = document.getElementById("security-form");
 
 document.addEventListener("DOMContentLoaded", function () {
     const links = document.querySelectorAll(".sidebar ul li a");
     const sections = document.querySelectorAll(".content-section");
-
-    // Profile Edit
-    editProfileBtn.addEventListener("click", showProfileEditOverlay);
-    profileEditCloseButton.addEventListener("click", hideProfileEditOverlay);
-    profileEditForm.addEventListener("submit", updateProfileInfo);
-
-    // Security Edit
-    updateSecurityBtn.addEventListener("click", showSecurityOverlay);
-    securityCloseButton.addEventListener("click", hideSecurityOverlay);
-    securityForm.addEventListener("submit", updateSecuritySettings);
 
     // Show the first section by default
     sections[0].classList.add("active");
@@ -44,27 +22,6 @@ document.addEventListener("DOMContentLoaded", function () {
             targetSection.classList.add("active");
         });
     });
-
-    // // Open the profile edit overlay when the "Edit Profile" button is clicked
-    // editProfileBtn.addEventListener("click", function () {
-    //   profileEditOverlay.style.display = "block";
-    //   console.log("Clicked Edit");
-    // });
-
-    // // Close the profile edit overlay when the close button is clicked
-    // closeButton.addEventListener("click", function () {
-    //   profileEditOverlay.style.display = "none";
-    // });
-
-    // // Handle the profile edit form submission
-    // profileEditForm.addEventListener("submit", function (event) {
-    //   event.preventDefault();
-    //   // Code to update the user's profile information
-    //   console.log("Profile information updated");
-    //   profileEditOverlay.style.display = "none";
-    //   // Show a custom pop-up notification
-    //   showSuccessPopup("Profile updated successfully!");
-    // });
 });
 
 function showProfileEditOverlay() {
